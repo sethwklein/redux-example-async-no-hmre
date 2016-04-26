214548383b38023ee4f6709b4a9ead261e6eb7d4
Seth W. Klein <sk@sethwklein.net>
2016-04-26 19:37:31 -0400

    Add files

---
+++ index.js
@@ -0,0 +1,41 @@
+import 'babel-polyfill'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import configureStore from './store/configureStore'
+
+const store = configureStore()
+const rootEl = document.getElementById('root')
+
+let render = () => {
+  const App = require('./containers/App').default
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    rootEl
+  )
+}
+
+if (module.hot) {
+  const renderApp = render
+  const renderError = (error) => {
+    const RedBox = require("redbox-react")
+    ReactDOM.render(
+      <RedBox error={error} />,
+      rootEl
+    )
+  }
+  render = () => {
+    try {
+      renderApp()
+    } catch (error) {
+      renderError(error)
+    }
+  }
+  module.hot.accept("./containers/App", () => {
+    setTimeout(render)
+  })
+}
+
+render()
